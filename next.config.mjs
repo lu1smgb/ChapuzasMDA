4398ed90a23de301da0f56fa0dac26f2721843c1
Juancho <jhernandezsanchezagesta@gmail.com>
2024-10-13 13:18:54 +0200

    Conexion a base de datos establecida , ya se puede insertar o consultar datos | desde PC casa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import 'dotenv/config';
+
+const nextConfig = {
+    reactStrictMode: true,
+};
 
 export default nextConfig;
+
+
---
+++ src/app/insercion/page.tsx
@@ -0,0 +1,36 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function Insercion() {
+    // Llama a la función de inserción aquí
+    const { data, error } = await supabase
+        .from("Juego")
+        .insert({
+            name: "VScode otroo",
+            site_url: "https://www.vscodeotroo.com",
+            img_name: "vscodeee.png",
+            bg_color: "bg-browwwn-400",
+        })
+        .select();
+
+    // Manejo de errores
+    if (error) {
+        console.error("Ha ocurrido un error al insertar el dato:", error.message);
+        return (
+            <div>
+                <h1>Inserción en la base de datos</h1>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    }
+
+    // Datos insertados
+    return (
+        <div>
+            <h1>Inserción en la base de datos</h1>
+            <pre>
+                Datos insertados:
+                {JSON.stringify(data, null, 2)}
+            </pre>
+        </div>
+    );
+}
