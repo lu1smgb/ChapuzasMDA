f4324ff860159c05105cc4f7ef06c4d55ba7bc79
Pablo Del Río López <127621833+pdrl02@users.noreply.github.com>
2024-10-26 18:18:48 +0200

    Update pullMain.sh

--- pullMain.sh
+++ pullMain.sh
@@ -1,6 +1,28 @@
-git checkout pablo
+#!/bin/bash
+
+# Verificar que se haya pasado un argumento
+if [ $# -ne 1 ]; then
+    echo "Uso: ./script.sh <nombre-de-la-rama>"
+    exit 1
+fi
+
+# Obtener el nombre de la rama de la línea de comandos
+branch_name=$1
+
+# Cambiar a la rama especificada
+git checkout "$branch_name"
+
+# Cambiar a la rama principal
 git checkout main
+
+# Hacer pull de la rama principal
 git pull origin main
-git checkout pablo
+
+# Regresar a la rama especificada
+git checkout "$branch_name"
+
+# Hacer merge de la rama principal
 git merge main
-# Resolver conflictos
+
+# Instrucciones para resolver conflictos si los hay
+echo "Resuelve los conflictos si hay."
